4ff19e6b89b1a441b2aa53063547c6b90f335cc1
Paola Lopes <paolaanacletolopes@gmail.com>
2022-11-30 14:09:08 -0300

    Menu Criado

--- src/ACMESpace.java
+++ src/ACMESpace.java
@@ -1,84 +1,93 @@
-import java.util.InputMismatchException;
-import java.util.Scanner;
+// import javafx.geometry.Insets;
+// import javafx.geometry.Pos;
+// import javafx.scene.Parent;
+// import javafx.scene.Scene;
+// import javafx.scene.control.Button;
+// import javafx.scene.control.Label;
+// import javafx.scene.control.PasswordField;
+// import javafx.scene.control.TextField;
+// import javafx.scene.layout.GridPane;
+// import javafx.scene.text.Font;
+// import javafx.scene.text.FontWeight;
+// import javafx.scene.text.Text;
 
-public class ACMESpace {
-    private Scanner entrada;
-    
+// public class ACMESpace extends GridPane {
+//     private App palco;
 
-    public ACMESpace() {
-        entrada = new Scanner(System.in);
-    }
+//     public ACMESpace(App palco) {
+//         super();
+//         this.palco = palco;
+//         setAlignment(Pos.CENTER);
+//         setHgap(10);
+//         setVgap(10);
+//         setPadding(new Insets(25, 25, 25, 25));
+//         setGridLinesVisible(false);
 
-    public void executa() {
-        int opcao;
-        boolean ok;
-        do {
-            ok = true;
-            menu();
-            try {
-                opcao = entrada.nextInt();
-                switch (opcao) {
-                    case 0:
-                        System.exit(0);
-                        break;
-                    case 1:
-                       
-                        break;
-                    case 2:
-                        
-                        break;
-                    case 3:
-                        
-                        break;
-                    case 4:
-                        
-                        break;
-                    case 5:
-                        
-                        break;
-                    case 6:
+//         Text scenetitle = new Text("HOME");
+//         scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 50));
+//         add(scenetitle, 8, 0, 2, 1);
 
-                        break;
-                    case 7:
+//         Button btnMuda1 = new Button("Cadastrar Espaço-Porto");
+//         Button btnMuda2 = new Button("Cadastrar Nova Espaçonave");
+//         Button btnMuda3 = new Button("Cadastrar Novo Transporte");
+//         Button btnMuda4 = new Button("Consultar Todos os Transportes");
+//         Button btnMuda5 = new Button("Alterar Estado de Transporte");
+//         Button btnMuda6 = new Button("Carregar Dados Iniciais");
+//         Button btnMuda7 = new Button("Designar Transportes");
+//         Button btnMuda8 = new Button("Salvar Dados");
+//         Button btnMuda9 = new Button("Carregar Dados");
+//         Button btnFinal = new Button("Finalizar Sistema");
 
-                        break;
-                    case 8:
+//         add(btnMuda1, 4, 3, 2, 1);
+//         add(btnMuda2, 8, 3, 2, 1);
+//         add(btnMuda3, 10, 3, 10, 1);
+//         add(btnMuda4, 4, 5, 3, 2);
+//         add(btnMuda5, 8, 5, 3, 2);
+//         add(btnMuda6, 10, 5, 3, 2);
+//         add(btnMuda7, 4, 7, 4, 3);
+//         add(btnMuda8, 8, 7, 4, 3);
+//         add(btnMuda9, 10, 7, 4, 3);
+//         add(btnFinal, 22, 4);
 
-                        break;
-                    case 9:
+//         btnMuda1.setOnAction(e -> {
+//              palco.mudaPainel(1);
+//          });
 
-                        break;
-                    default:
-                        System.out.println("Opção inválida.");
-                        executa();
-                }
-            } catch (InputMismatchException e1) {
-                entrada.nextLine();
-                ok = false;
-                System.out.println("Tipo incorreto. Redigite.");
-            } catch (Exception e2) {
-                entrada.nextLine();
-                ok = false;
-                e2.printStackTrace();
-                System.out.println("Redigite.");
-            }
-        } while (!ok);
-    }
+//          btnMuda2.setOnAction(e -> {
+//             palco.mudaPainel(2);
+//         });
 
-    private void menu() {
-        System.out.println("=============================");
-        System.out.println("Opcões:");
-        System.out.println("[0] Sair do sistema");
-        System.out.println("[1] Cadastrar Novo Espaço-Porto");
-        System.out.println("[2] Cadastrar Nova Espaçonave");
-        System.out.println("[3] Cadastrar Novo Transporte");
-        System.out.println("[4] Consultar Todos Transportes");
-        System.out.println("[5] Alterar Estado de Transporte");
-        System.out.println("[6] Carregar Dados Iniciais");
-        System.out.println("[7] Designar Transportes");
-        System.out.println("[8] Salvar Dados");
-        System.out.println("[9] Carregar Dados");
-        System.out.println("Opcão desejada: ");
-    }
-    
-}
+//         btnMuda3.setOnAction(e -> {
+//             palco.mudaPainel(2);
+//         });
+
+//         btnMuda4.setOnAction(e -> {
+//             palco.mudaPainel(2);
+//         });
+
+//         btnMuda5.setOnAction(e -> {
+//             palco.mudaPainel(2);
+//         });
+
+//         btnMuda1.setOnAction(e -> {
+//             palco.mudaPainel(2);
+//         });
+
+//         btnMuda1.setOnAction(e -> {
+//             palco.mudaPainel(2);
+//         });
+
+//         btnMuda1.setOnAction(e -> {
+//             palco.mudaPainel(2);
+//         });
+
+//         btnMuda1.setOnAction(e -> {
+//             palco.mudaPainel(2);
+//         });
+
+//         btnFinal.setOnAction(e -> {
+//             palco.fecha();
+//         });
+//     }
+
+// }
---
+++ src/Painel1.java
@@ -0,0 +1,73 @@
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+
+public class Painel1 extends GridPane {
+//     private App palco;
+//     private EspacoPortoU epu;
+
+//     public Painel1(App palco) {
+//         super();
+//         this.palco = palco;
+//         this.epu = epu;
+//         setAlignment(Pos.CENTER);
+//         setHgap(10);
+//         setVgap(10);
+//         setPadding(new Insets(25, 25, 25, 25));
+//         setGridLinesVisible(false);
+
+//         Text scenetitle = new Text("Painel 1");
+//         scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+//         add(scenetitle, 0, 0, 2, 1);
+
+//         Label numero = new Label("Número do Espaço-Porto:");
+//         add(numero, 0, 1);
+
+//         TextField userTextField = new TextField();
+//         add(userTextField, 1, 1);
+
+//         Label nome = new Label("Nome do Espaço-Porto:");
+//         add(nome, 0, 2);
+
+//         TextField userTextField = new TextField();
+//         add(userTextField, 1, 1);
+
+//         numero = new Label();
+//         add(numero, 0, 1, 2, 1);
+//         nome = new Label();
+//         add(nome, 0, 2, 2, 1);
+//         coordX = new Label();
+//         add(coordX, 0, 2, 2, 1);
+//         coordY = new Label();
+//         add(coordY, 0, 2, 2, 1);
+//         coordZ = new Label();
+//         add(coordZ, 0, 2, 2, 1);
+
+//         Button btnMuda = new Button("Mudar para Cena 1");
+//         Button btnFinal = new Button("Terminar");
+
+//         add(btnMuda, 0, 3);
+//         add(btnFinal, 0, 4);
+
+//         // Tratamento de evento dos botoes
+//         btnMuda.setOnAction(e -> {
+//             palco.mudaPainel(1);
+//         });
+
+//         btnFinal.setOnAction(e -> {
+//             palco.fecha();
+//         });
+//     }
+
+//     // Atualizacao de componentes do painel 2
+//     // public void atualiza() {
+//     //     userName.setText("Nome do usuario: " + usuario.getUser());
+//     //     pw.setText("Senha: " + usuario.getPassword());
+//     // }
+ }

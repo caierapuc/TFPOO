5315c39e314dba7aacb86bf11c2c6feff8c45c53
caierapuc <caio.miguel@edu.pucrs.br>
2022-11-29 21:39:59 -0300

    Project structure

---
+++ src/Controllers/EspaconavesController.java
@@ -0,0 +1,38 @@
+package Controllers;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import Entities.*;
+import Repositories.EspaconavesRepository;
+
+public class EspaconavesController {
+    private final EspaconavesRepository espaconavesRepository;
+
+    public EspaconavesController(EspaconavesRepository _espaconavesRepository) {
+        this.espaconavesRepository = _espaconavesRepository;
+    }
+
+    public boolean carregarDadosIniciais(String path) throws Exception {
+        File file = new File(path);
+
+        if (!file.exists())
+            throw new FileNotFoundException("Arquivo não encontrado!");
+
+        try (Scanner fr = new Scanner(file)) {
+            int count = 0;
+            while (fr.hasNextLine()){
+                String[] temp = fr.nextLine().split(";");
+                if (count != 0){
+                    espaconavesRepository.addEspaconave();
+                }
+                else
+                    count++;
+            }
+            fr.close();
+        }
+
+        return true;
+    }
+}
---
+++ src/Repositories/EspaconavesRepository.java
@@ -0,0 +1,13 @@
+package Repositories;
+
+import java.util.ArrayList;
+
+import Entities.Espaconave;
+
+public class EspaconavesRepository {
+    private ArrayList<Espaconave> espaconaves = new ArrayList<Espaconave>();
+    
+    public void addEspaconave() {
+    }
+    
+}
